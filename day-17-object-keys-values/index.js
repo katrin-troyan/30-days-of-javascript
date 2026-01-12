ffb00cb74a0d6e246621c10218151d9b674711c1
Kateryna Troyan <ket.tro@gmail.com>
2026-01-12 21:30:53 +0200

    Day 17: Object.keys, values and entries

---
+++ day-17-object-keys-values/index.js
@@ -0,0 +1,36 @@
+/*Task 1*/
+//Виведи всі ключі в консоль
+// const user = {
+//   name: "Anna",
+//   age: 22,
+//   isOnline: true,
+// };
+// console.log(Object.keys(user));
+
+/*Task 2*/
+//Виведи всі значення
+// console.log(Object.values(user));
+
+/*Task 3*/
+//Перебір entries
+// console.log(Object.entries(user));
+
+/*Task 4*/
+//Напиши функцію countProps(obj), яка повертає кількість властивостей обʼєкта
+// function countProps(obj) {
+//   return Object.keys(obj).length;
+// }
+
+/*Task 5*/
+//Отримай масив предметів, де оцінка ≥ 80 з деструктуризацією
+const scores = {
+  math: 90,
+  english: 75,
+  physics: 85,
+  history: 60,
+};
+const bestScores = Object.entries(scores)
+  .filter(([subject, score]) => score >= 80)
+  .map(([subject]) => subject);
+
+console.log(bestScores);
